7f1cb9f5b77a42b2708eb5d705d0fb9cf8a6a4e3
Gabriel Hayon <gabriel.hayon@despegar.com>
2019-11-20 21:39:16 -0300

    Added BaseDeDatos.h

---
+++ src/BaseDeDatos.h
@@ -0,0 +1,15 @@
+#ifndef TP3_BASEDEDATOS_H
+#define TP3_BASEDEDATOS_H
+
+#include <map>
+
+#include "Tabla.h"
+#include "Tipos.h"
+
+class BaseDeDatos {
+public:
+
+private:
+};
+
+#endif //TP3_BASEDEDATOS_H
