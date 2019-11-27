2d0aa49ec5c25cab9f5469de8d32d0ccef30d20b
Gabriel Hayon <gabriel.hayon@despegar.com>
2019-11-26 23:27:04 -0300

    Added tabla tests

--- tests/tabla_tests.cpp
+++ tests/tabla_tests.cpp
@@ -1,9 +1,116 @@
 #include "gtest-1.8.1/gtest.h"
 #include "../src/Tabla.h"
+#include <vector>
+
+linear_set<NombreCampo> getFieldsToTest() {
+    linear_set<NombreCampo> fields = linear_set<NombreCampo>();
+
+    fields.insert("Nombre");
+    fields.insert("Apellido");
+    fields.insert("Domicilio");
+    fields.insert("DNI");
+
+    return fields;
+}
+
+NombreCampo getKeyToTest() {
+    return "DNI";
+}
+
+Tabla initializeTable() {
+    return Tabla(getFieldsToTest(), getKeyToTest());
+}
+
+Registro createRegister(vector<NombreCampo> values) {
+    vector<NombreCampo> fields = {"Nombre", "Apellido", "Domicilio", "DNI"};
+    Registro reg;
+
+    for (int i = 0; i < 4; ++i) {
+        reg.definir(fields[i], values[i]);
+    }
+
+    return reg;
+}
 
 TEST(tabla_test, vacio) {
-    string_map<int> vacio;
+    Tabla table = initializeTable();
 
-    EXPECT_TRUE(vacio.empty());
-    EXPECT_EQ(vacio.count("hola"), 0);
+    EXPECT_EQ(table.registros().size(), 0);
+    EXPECT_EQ(table.campos(), getFieldsToTest());
+    EXPECT_EQ(table.clave(), getKeyToTest());
+}
+
+TEST(tabla_test, insertar_y_borrar) {
+
+    Tabla table = initializeTable();
+
+    Registro reg1 = createRegister({"Gabriel", "Hayon", "Bs as", "40729302"});
+    Registro reg2 = createRegister({"Nacho1", "Alonso", "Bs as", "12345678"});
+    Registro reg3 = createRegister({"Nacho2", "Crinigan", "Bs as", "87654321"});
+
+    linear_set<Registro> regs;
+
+    table.insertar(reg1);
+    regs.insert(reg1);
+
+    EXPECT_EQ(table.registros().size(), 1);
+    EXPECT_EQ(table.registros(), regs);
+
+    table.insertar(reg2);
+    regs.insert(reg2);
+
+    EXPECT_EQ(table.registros().size(), 2);
+    EXPECT_EQ(table.registros(), regs);
+
+    table.insertar(reg3);
+    regs.insert(reg3);
+
+    EXPECT_EQ(table.registros().size(), 3);
+    EXPECT_EQ(table.registros(), regs);
+
+    table.borrar("12345678"); //reg2
+    regs.erase(reg2);
+
+    EXPECT_EQ(table.registros().size(), 2);
+    EXPECT_EQ(table.registros(), regs);
+
+    table.borrar("40729302"); //reg1
+    regs.erase(reg1);
+
+    EXPECT_EQ(table.registros().size(), 1);
+    EXPECT_EQ(table.registros(), regs);
+
+    table.borrar("87654321"); //reg3
+    regs.erase(reg3);
+
+    EXPECT_EQ(table.registros().size(), 0);
+    EXPECT_EQ(table.registros(), regs);
+
+    EXPECT_EQ(table.campos(), getFieldsToTest());
+    EXPECT_EQ(table.clave(), getKeyToTest());
+}
+
+TEST(tabla_test, insertar_sobreescribir) {
+    Tabla table = initializeTable();
+
+    linear_set<Registro> regs;
+
+    Registro reg1 = createRegister({"Gabriel", "Hayon", "Bs as", "40729302"});
+    Registro reg2 = createRegister({"Nacho1", "Alonso", "Bs as", "40729302"});
+
+    table.insertar(reg1);
+    regs.insert(reg1);
+
+    EXPECT_EQ(table.registros().size(), 1);
+    EXPECT_EQ(table.registros(), regs);
+
+    table.insertar(reg2);
+    regs.erase(reg1);
+    regs.insert(reg2);
+
+    EXPECT_EQ(table.registros().size(), 1);
+    EXPECT_EQ(table.registros(), regs);
+
+    EXPECT_EQ(table.campos(), getFieldsToTest());
+    EXPECT_EQ(table.clave(), getKeyToTest());
 }
